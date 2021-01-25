8a63cac27b85f4bccfb69116f8a5fb154dacd008
Murat Topallar <topallarm@outlook.com>
2021-01-25 18:41:10 +0300

    yorum satırları eklendi

--- OyunProject/Concrete/MusteriDogrulamaManager.cs
+++ OyunProject/Concrete/MusteriDogrulamaManager.cs
@@ -10,7 +10,10 @@ namespace OyunProject.Concrete
     {
         public bool MusteriDogrula(Musteri musteri)
         {
+            // Mernis sisteminden doğrulama yaptığımızı varsayıyoruz. Buradan true dönerse müşteri kaydı gerçekleşiyor, false dönerse kayıt gerçekleşmiyor ve bir bilgilendirme mesajı alıyoruz.
+            
             return true;
+
             //return false;
         }
     }
